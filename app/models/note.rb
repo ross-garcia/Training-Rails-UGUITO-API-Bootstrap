ac3d6d0a13116f4f44391bb324f53f7c537b5e93
ross-garcia <160248891+ross-garcia@users.noreply.github.com>
2024-02-27 16:41:43 -0300

    feat: [TD-292] validations Note model (#3)
    * feat: validations Note model
    - added word_count, content_length, content_length_of_south_utility and content_length_of_north_utility methods to Note model
    - added seeds for Note (modified seed.rb file)
    - custom message in locales files
    
    * feat: validations Note model
    - NorthUtility and SouthUtility modified: content_length_short and content_length_medium methods added
    - Note model modified: validate_content_length method added
    - custom message added to validate_content_length
    - content_length method modified
    
    * feat: test to verify the relationship with the User model deleted

--- app/models/note.rb
+++ app/models/note.rb
@@ -12,8 +12,33 @@
 #
 class Note < ApplicationRecord
   enum note_type: { review: 0, critique: 1 }
-  validates :title, :content, :note_type, :user_id, presence: true
+  validates :title, :content, :note_type, presence: true
+  validate :validate_content_length
 
   belongs_to :user
   has_one :utility, through: :user
+
+  def word_count
+    content.to_s.split.length
+  end
+
+  def content_length
+    return 'short' if word_count <= utility.content_length_short
+    return 'medium' if word_count <= utility.content_length_medium
+    'long'
+  end
+
+  private
+
+  def validate_content_length
+    errors.add :content, validate_content_length_error if invalid_content?
+  end
+
+  def validate_content_length_error
+    I18n.t('activerecord.errors.models.note.attributes.content.validate_content_length')
+  end
+
+  def invalid_content?
+    word_count > utility.content_length_short && review?
+  end
 end
--- app/models/south_utility.rb
+++ app/models/south_utility.rb
@@ -1,2 +1,9 @@
 class SouthUtility < Utility
+  def content_length_short
+    60
+  end
+
+  def content_length_medium
+    120
+  end
 end
