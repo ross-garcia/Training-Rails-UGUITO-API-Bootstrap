d016d396b46df1ad05f420ff0a125b36566cc08d
ross-garcia <rossmery.garcia@widergy.com>
2024-03-04 17:45:38 -0300

    feat: tests added - index service - show service

---
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -0,0 +1,128 @@
+describe Api::V1::NotesController, type: :controller do
+  describe 'GET #index' do
+    let(:user_notes) { create_list(:note, 5, user: user) }
+    let(:allowed_keys) { %w[id title note_type content_length] }
+
+    context 'when there is a user logged in' do
+      include_context 'with authenticated user'
+
+      let!(:expected) do
+        ActiveModel::Serializer::CollectionSerializer.new(
+          notes_expected,
+          serializer: IndexNoteSerializer
+        ).to_json
+      end
+
+      context 'when fetching all the notes for user' do
+        let(:notes_expected) { user_notes }
+
+        before { get :index }
+
+        it 'responds with the expected notes json' do
+          expect(response_body.count).to eq(JSON.parse(expected).count)
+        end
+
+        it 'responds matches with allowed keys' do
+          expect(response_body.first.keys).to match_array(allowed_keys)
+        end
+
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'when fetching books with page and page size params' do
+        let(:page)            { 1 }
+        let(:page_size)       { 2 }
+        let(:notes_expected) { user_notes.first(2) }
+
+        before { get :index, params: { page: page, page_size: page_size } }
+
+        it 'responds with the expected number of notes' do
+          expect(response_body.count).to eq(page_size)
+        end
+
+        it 'responds matches with allowed keys' do
+          expect(response_body.first.keys).to match_array(allowed_keys)
+        end
+
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'when fetching notes using filters' do
+        let(:note_type) { 'review' }
+
+        let!(:notes_custom) { create_list(:note, 2, user: user, note_type: note_type) }
+        let(:notes_expected) { notes_custom }
+
+        before { get :index, params: { note_type: note_type } }
+
+        it 'responds with expected notes' do
+          response_body.each { |note| expect(note['note_type']).to eq(note_type) }
+        end
+
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
+        end
+      end
+    end
+
+    context 'when there is not a user logged in' do
+      context 'when fetching all the notes for user' do
+        before { get :index }
+
+        it_behaves_like 'unauthorized'
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    let(:allowed_keys) { %w[id title note_type word_count created_at content user content_length] }
+
+    context 'when there is a user logged in' do
+      include_context 'with authenticated user'
+
+      let(:expected) { ShowNoteSerializer.new(note, root: false).to_json }
+
+      context 'when fetching a valid note' do
+        let(:note) { create(:note, user: user) }
+
+        before { get :show, params: { id: note.id } }
+
+        it 'responds with the note json' do
+          expect(response.body).to eq(expected)
+        end
+
+        it 'responds matches with allowed keys' do
+          expect(response_body.keys).to match_array(allowed_keys)
+        end
+
+        it 'responds with a note belonging to the logged in user' do
+          expect(response_body.fetch('user').fetch('id')).to eq(user.id)
+        end
+
+        it 'responds with 200 status' do
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'when fetching a invalid note' do
+        before { get :show, params: { id: Faker::Number.number } }
+
+        it 'responds with 404 status' do
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+    end
+
+    context 'when there is not a user logged in' do
+      context 'when fetching an note' do
+        before { get :show, params: { id: Faker::Number.number } }
+
+        it_behaves_like 'unauthorized'
+      end
+    end
+  end
+end
