6ba791e9e7eade525fe8518d853d459f86beccd5
ross-garcia <rossmery.garcia@widergy.com>
2024-03-05 08:29:35 -0300

    feat: index and show methods modified

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,34 +1,55 @@
 module Api
   module V1
     class NotesController < ApplicationController
+      rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+
       def index
-        render json: notes_filtered, status: :ok, each_serializer: IndexNoteSerializer
+        render json: notes, status: :ok, each_serializer: IndexNoteSerializer
       end
 
       def show
-        render json: show_note, status: :ok, serializer: ShowNoteSerializer
+        render json: note, status: :ok, serializer: ShowNoteSerializer
       end
 
       private
 
-      def notes
+      def all_notes
         Note.all
       end
 
       def filtering_params
+        params[:note_type] = note_type
         params.permit(%i[note_type])
       end
 
-      def notes_filtered
-        order = params[:order].in?(%w[asc desc ASC DESC]) ? params[:order] : 'desc'
-
-        Note.where(filtering_params)
-            .order(created_at: order)
-            .page(params[:page])
-            .per(params[:page_size])
+      def note_type
+        valid_note_type? ? params[:note_type] : nil
       end
 
-      def show_note
+      def valid_note_type?
+        Note.note_types.keys.include?(params[:note_type])
+      end
+
+      def notes
+        all_notes.where(filtering_params)
+                 .order(created_at: order)
+                 .page(params[:page])
+                 .per(params[:page_size])
+      end
+
+      def order
+        params[:order].in?(%w[asc desc ASC DESC]) ? params[:order] : 'desc'
+      end
+
+      def render_record_not_found
+        render json: { error: not_found_error }, status: :not_found
+      end
+
+      def not_found_error
+        I18n.t 'errors.render_record_not_found'
+      end
+
+      def note
         Note.find(params.require(:id))
       end
     end
