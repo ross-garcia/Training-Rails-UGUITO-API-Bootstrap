6b410d91abe8f4e0f6e1d1997d806ad3ae2d84a0
ross-garcia <rossmery.garcia@widergy.com>
2024-03-06 08:11:15 -0300

    feat: note creation service added

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -11,6 +11,15 @@ module Api
         render json: show_note, status: :ok, serializer: ShowNoteSerializer
       end
 
+      def create
+        create_note
+        render json: { message: 'Nota creada con éxito.' }, status: :created
+      rescue ActiveRecord::RecordInvalid => e
+        render_invalid_content if e.record.errors.to_h[:content]
+      rescue ArgumentError
+        render_invalid_note_type unless valid_note_type?
+      end
+
       private
 
       def notes
@@ -32,6 +41,39 @@ module Api
 
       def show_note
         notes.find(params.require(:id))
+      end
+
+      def create_note
+        Note.create!(note_create_params.merge(user_id: current_user.id))
+      end
+
+      def note_create_params
+        params.require(:note).require(%i[title content note_type])
+        params.require(:note).permit(%i[title content note_type]).to_h
+      end
+
+      def render_invalid_note_type
+        render json: { error: invalid_note_type_error }, status: :unprocessable_entity
+      end
+
+      def invalid_note_type_error
+        I18n.t 'errors.render_invalid_note_type'
+      end
+
+      def valid_note_type?
+        Note.note_types.keys.include?(note_type)
+      end
+
+      def render_invalid_content
+        render json: { error: invalid_content_error }, status: :unprocessable_entity
+      end
+
+      def invalid_content_error
+        I18n.t 'errors.render_invalid_content'
+      end
+
+      def note_type
+        note_create_params[:note_type]
       end
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActionController::ParameterMissing, with: :render_missing_parameters
 
   private
 
@@ -52,5 +53,13 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name document_number])
+  end
+
+  def render_missing_parameters
+    render json: { error: missing_parameters_error }, status: :bad_request
+  end
+
+  def missing_parameters_error
+    I18n.t 'errors.render_missing_parameters'
   end
 end
