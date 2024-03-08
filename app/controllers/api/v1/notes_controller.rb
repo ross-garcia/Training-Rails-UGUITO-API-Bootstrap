1edca5bbd3fb063a4e638897a57480a51eb811be
ross-garcia <rossmery.garcia@widergy.com>
2024-03-08 20:07:57 -0300

    feat: resolution of code conflicts and corrections in the note controller and tests

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -2,9 +2,10 @@ module Api
   module V1
     class NotesController < ApplicationController
       before_action :authenticate_user!
+      before_action :note_create_params_required, only: [:create]
 
       def index
-        return render_invalid_filter_note_type unless valid_note_type?
+        return render_invalid_note_type unless valid_note_type_filter?
         render json: notes, status: :ok, each_serializer: IndexNoteSerializer
       end
 
@@ -13,74 +14,78 @@ module Api
       end
 
       def create
-        create_note
-        render json: { message: 'Nota creada con éxito.' }, status: :created
+        return render_invalid_note_type unless valid_note_type?
+        return render json: { message: 'Nota creada con éxito.' }, status: :created if create_note
       rescue ActiveRecord::RecordInvalid => e
         render_invalid_content if e.record.errors.to_hash[:content]
-      rescue ArgumentError
-        render_invalid_note_type unless valid_note_type?
+      rescue StandardError => e
+        render json: { error: e.message }, status: :internal_server_error
       end
 
       private
 
-      def current_user_notes
-        current_user.notes
+      def notes
+        current_user.notes.where(filtering_params)
+                    .order(created_at: order)
+                    .page(params[:page])
+                    .per(params[:page_size])
       end
 
       def filtering_params
         params.permit [:note_type]
       end
 
-      def note_type
+      def note_type_filter
         params[:note_type]
-      end
-
-      def valid_note_type?
-        note_type.nil? || Note.note_types.keys.include?(note_type)
-      end
-
-      def notes
-        current_user_notes.where(filtering_params)
-                          .order(created_at: order)
-                          .page(params[:page])
-                          .per(params[:page_size])
       end
 
       def order
         params[:order].in?(%w[asc desc ASC DESC]) ? params[:order] : 'desc'
       end
 
-      def render_invalid_filter_note_type
-        render json: { error: invalid_filter_note_type_error }, status: :bad_request
+      def valid_note_type_filter?
+        note_type_filter.nil? || Note.note_types.keys.include?(note_type_filter)
       end
 
-      def invalid_filter_note_type_error
-        I18n.t 'errors.render_invalid_filter_note_type'
+      def render_invalid_note_type
+        render json: { error: invalid_note_type_error }, status: :unprocessable_entity
+      end
+
+      def invalid_note_type_error
+        I18n.t 'errors.render_invalid_note_type'
       end
 
       def note
-        current_user_notes.find(params.require(:id))
+        current_user.notes.find(params.require(:id))
       end
 
       def create_note
-        Note.create!(note_create_params.merge(user_id: current_user.id))
+        Note.create!(note_create_params_required.merge(user_id: current_user.id))
       end
 
-      def note_create_params
+      def note_create_params_required
         params.require(:note).require(%i[title content note_type])
         params.require(:note).permit(%i[title content note_type]).to_h
+      end
+
+      def note_type
+        note_create_params_required[:note_type]
+      end
+
+      def valid_note_type?
+        Note.note_types.keys.include?(note_type)
       end
 
       def render_invalid_content
         render json: { error: invalid_content_error }, status: :unprocessable_entity
       end
 
-      def invalid_content_error
-        I18n.t 'errors.render_invalid_content'
+      def limit_review_content_length
+        current_user.utility.content_length_short
       end
 
-      def body_note_type
-        note_create_params[:note_type]
+      def invalid_content_error
+        I18n.t 'errors.render_invalid_content', limit: limit_review_content_length
       end
     end
   end
