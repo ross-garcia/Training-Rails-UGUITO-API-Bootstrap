8ce9e195a2f17f059bfadde6bc336d25956f32b6
ross-garcia <rossmery.garcia@widergy.com>
2024-03-13 11:36:57 -0300

    feat: admin dashboard for books

---
+++ app/admin/books.rb
@@ -0,0 +1,44 @@
+ActiveAdmin.register Book do
+  includes :utility, :user
+  permit_params :title, :author, :genre, :image, :publisher, :year, :utility_id, :user_id
+
+  config.per_page = 5
+
+  index do
+    selectable_column
+    id_column
+    column 'User' do |book|
+      "#{book.user.first_name} #{book.user.last_name}"
+    end
+    column :utility
+    column :title
+    column :author
+    column :genre
+    actions
+  end
+
+  filter :title
+  filter :author
+  filter :genre
+  filter :created_at
+  filter :updated_at
+
+  form do |f|
+    f.inputs 'Book Details', allow_destroy: true do
+      f.semantic_errors(*f.object.errors.keys)
+      f.input :title
+      f.input :author
+      f.input :genre
+      f.input :image, as: :url
+      f.input :publisher
+      f.input :year
+      f.input :user_id, label: 'User', as: :select, collection: User.all.map { |user|
+        ["#{user.first_name} #{user.last_name}", user.id]
+      }
+      f.input :utility_id, label: 'Utility', as: :select, collection: Utility.all.map { |utility|
+        [utility.type, utility.id]
+      }
+      f.actions
+    end
+  end
+end
